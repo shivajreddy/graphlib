5e556440aa103101b1644d0d3a4b73d6e66e73af
shiva <sreddy@hey.com>
2025-09-18 10:27:28 -0400

    .

--- graph.cpp
+++ graph.cpp
@@ -1,52 +1,57 @@
-/*
+#include <bits/stdc++.h>
+using namespace std;
 
-   Graph -> [direct, undirect] -> [weighted, unweighted]
-
-   1. input: {1, 2, 3}, {2, 3, 2}
-   2. input: "[1, 2, 3], [2, 1, 3]"
-*/
-
-#include <utility>
-#include <vector>
-using std::vector;
-#include <string>
-using std::pair;
-using std::string;
-using std::tuple;
-
-// UNWEIGHTED GRAPH
 class Graph {
 protected:
     int total_vertices;
-
-    vector<vector<int>> adj;
-
-public:
-    /** Data in the shape of Vec<pair<int,int>> */
-    Graph(vector<pair<int, int>> edges) { // unweighted graph, bidirectional
-        int n = edges.size();
-
-        // build the adj list
-        adj.resize(n);
-        for (auto [u, v] : edges) adj[u].push_back(v);
-    }
-
-    /** Data in the shape of Vec<tuple<int, int, int>> */
-    Graph(vector<tuple<int, int, int>> data) { // weighted graph
-    }
-
-    Graph(string data) {
-        // create an adj list
-        // decide if this is weighted or unweighted
-    }
 };
 
 class UnDirectedGraph : public Graph {
-protected:
-    vector<vector<int>> adj;
+public:
+    /** Un-Weighted Graph */
+    using T_vpii = vector<pair<int, int>>;
+    UnDirectedGraph(T_vpii edges) {
+        /* Example input: [{1, 2}, {1, 3}, {2, 4}] */
+        int n = edges.size();
+        total_vertices = n;
+        // build the adj matrix
+        vector<vector<bool>> adj_mat(n, vector<bool>(n, false));
+        for (auto [u, v] : edges) {
+            adj_mat[u][v] = true;
+            adj_mat[v][u] = true;
+        }
+    }
+
+    /** Weighted Graph */
+    using T_vtiii = vector<tuple<int, int, int>>;
+    UnDirectedGraph(T_vtiii edges) {
+        int n = edges.size();
+        total_vertices = n;
+
+        // [u1 : [{v1,w1}, {v2,w2},...], u2 : [], ... ]
+        vector<vector<pair<int, int>>> adj(n);
+
+        for (auto [u, v, w] : edges) {
+            adj[u].push_back({ v, w });
+            adj[v].push_back({ u, w });
+        }
+    }
+
+    UnDirectedGraph(string data) {
+        // create an adj list
+    }
 };
 
 class DirectedGraph : public Graph {
 protected:
     vector<vector<pair<int, int>>> adj;
+
+public:
+    using T_vpii = vector<pair<int, int>>;
+    DirectedGraph(T_vpii edges) {
+    }
+
+    using T_vtiii = vector<tuple<int, int, int>>;
+    DirectedGraph(T_vtiii data) {
+    }
 };
